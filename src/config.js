ded4e38e0e0d2edb7fdebaaec40389976d1f4ae2
olivio-git <subelzaolivitocabezas@gmail.com>
2022-12-20 12:50:17 -0400

    bd actualizada

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const PORT=process.env.PORT || 3001;
 
-export const DB_USER=process.env.DB_USER || 'mvbpklin1h8b5cvszzzz';
+export const DB_USER=process.env.DB_USER || 'gf1w0iypq9lg99m54cyz';
 export const DB_HOST=process.env.DB_HOST || 'us-east.connect.psdb.cloud'
-export const DB_PASSWORD=process.env.DB_PASSWORD || 'pscale_pw_10CKzu1QfNIjqcxXq4oAAOB7MoLO7CoHZL75kTXC5eg'
+export const DB_PASSWORD=process.env.DB_PASSWORD || 'pscale_pw_7Q1Ow1Eywx5nFWGoOCMcUDBlekrbvQ5LtHmOtpYhdix'
 export const DB_NAME=process.env.DB_NAME || 'personas'
 export const DB_PORT=process.env.DB_PORT || 3306;
